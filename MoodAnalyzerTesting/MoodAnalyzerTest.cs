2762cb22e864f91b85e26239ad9f1d1debf24025
naveenveera789 <naveenveera789@gmail.com>
2021-12-01 19:08:04 +0530

    [naveenveera789] add : The Test case 1.2 program is done

--- MoodAnalyzerTesting/MoodAnalyzerTest.cs
+++ MoodAnalyzerTesting/MoodAnalyzerTest.cs
@@ -18,5 +18,12 @@ namespace MoodAnalyzerTesting
             string result = mood.AnalyzeMood("I am in Sad Mood");
             Assert.AreEqual(output, result);
         }
+        [Test]
+        public void GivenAnyMessage_WhenAnalyze_ShouldReturnHappy()
+        {
+            string output = "HAPPY";
+            string result = mood.AnalyzeMood("I am in Any Mood");
+            Assert.AreEqual(output, result);
+        }
     }
 }
