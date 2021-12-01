0e1fab0f1af56e24b61bff0caca950df963a2a20
naveenveera789 <naveenveera789@gmail.com>
2021-12-01 18:51:40 +0530

    [naveenveera789] add : The analyse and response mood program is done

---
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if(this.message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
