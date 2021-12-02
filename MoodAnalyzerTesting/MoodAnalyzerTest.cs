488e83f9ab374e5bc879f8688c2c3054275889ea
naveenveera789 <naveenveera789@gmail.com>
2021-12-02 13:17:00 +0530

    [naveenveera789] add : The Test case 3.2 program is done

--- MoodAnalyzerTesting/MoodAnalyzerTest.cs
+++ MoodAnalyzerTesting/MoodAnalyzerTest.cs
@@ -5,32 +5,64 @@ namespace MoodAnalyzerTesting
 {
     public class Tests
     {
-        MoodAnalyzer mood;
         [SetUp]
         public void Setup()
         {
-            this.mood = new MoodAnalyzer();
         }
         [Test]
         public void GivenSadMessage_WhenAnalyze_ShouldReturnSad()
         {
+            string message = "I am in Sad Mood";
             string output = "SAD";
-            string result = mood.AnalyzeMood("I am in Sad Mood");
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
             Assert.AreEqual(output, result);
         }
         [Test]
         public void GivenAnyMessage_WhenAnalyze_ShouldReturnHappy()
         {
+            string message = "I am in Any Mood";
             string output = "HAPPY";
-            string result = mood.AnalyzeMood("I am in Any Mood");
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
             Assert.AreEqual(output, result);
         }
         [Test]
         public void GivenNullMessage_WhenAnalyze_ShouldReturnHappy()
         {
+            string message = null;
             string output = "HAPPY";
-            string result = mood.AnalyzeMood("NULL");
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
             Assert.AreEqual(output, result);
+        }
+        [Test]
+        public void GivenNullMood_WhenAnalyze_ShouldThrowInvalidMoodException()
+        {
+            try
+            {
+                string message = null; 
+                MoodAnalyzer mood = new MoodAnalyzer(message);
+                string result = mood.AnalyzeMood();
+            }
+            catch(MoodAnalyzerException e)
+            {
+                Assert.AreEqual("Mood should not be Null", e.Message);            
+            }
+        }
+        [Test]
+        public void GivenEMPTYMood_WhenAnalyze_ShouldThrowInvalidMoodException()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyzer mood = new MoodAnalyzer(message);
+                string result = mood.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException e)
+            {
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
         }
     }
 }
